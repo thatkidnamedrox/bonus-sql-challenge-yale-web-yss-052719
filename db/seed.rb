20056281a714ac9c38c9455451f15bb8788239f3
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 14:17:34 +0000

    Automatically backed up by Learn

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,59 @@
 # Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+require 'csv'
+require_relative '../config/environment.rb'
+
+class Daily_Show
+  @@all = []
+  attr_accessor :year, :googleknowledge_occupation, :show, :group, :raw_guest_list
+
+  def initialize(attributes, id=nil)
+    @id = id
+    attributes.each {|key, value| self.send("#{key}=", value) }
+    @@all << self
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS daily_show_guests (
+        id INTEGER PRIMARY KEY,
+        year INTEGER,
+        googleknowledge_occupation TEXT,
+        show TEXT,
+        gr TEXT,
+        raw_guest_list TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO daily_show_guests (year, googleknowledge_occupation, show, gr, raw_guest_list)
+      VALUES (?,?,?,?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.year, self.googleknowledge_occupation, self.show, self.group, self.raw_guest_list)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM daily_show_guests")[0][0]
+  end
+end
+
+
+
+table = CSV.read("./daily_show_guests.csv",
+headers: true);
+
+Daily_Show.create_table
+
+
+table.each do |row|
+  attributes = {
+    year: row["YEAR"],
+    googleknowledge_occupation: row["GoogleKnowlege_Occupation"],
+    show: row["Show"],
+    group: row["Group"],
+    raw_guest_list: row["Raw_Guest_List"]
+  }
+  daily_show = Daily_Show.new(attributes);
+  daily_show.save;
+end
--- lib/queries.rb
+++ lib/queries.rb
@@ -1,5 +1,22 @@
 # Write methods that return SQL queries for each part of the challeng here
+require_relative '../config/environment.rb'
 
 def guest_with_most_appearances
   # write your query here!
+  """
+  SELECT raw_guest_list
+  FROM daily_show_guests
+  ORDER BY COUNT(raw_guest_list) DESC LIMIT 1
+  ;
+  """
 end
+
+def most_popular_profession_for_each_year
+  """
+  SELECT googleknowledge_occupation
+  FROM daily_show_guests
+  """
+end
+
+binding.pry
+0
